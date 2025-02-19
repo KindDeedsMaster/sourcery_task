021a8a50615bbe929d98c55248ed7c85bd9985cd
Anton Isakov <anton.isakov23@gmail.com>
2025-02-19 02:47:13 +0200

    Methot for string decompression created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println(stringCompression("aabaaac"));
+        System.out.println(stringDecompression("a2b1a3c1"));
     }
 
-    public static String stringCompression(String stringToCompress) {
+    private static String stringCompression(String stringToCompress) {
         // If string is empty returning empty string.
         int stringLength = stringToCompress.length();
         if (stringLength == 0) {
@@ -35,4 +36,24 @@ public class Main {
         }
         return result;
     }
+
+    private static String stringDecompression (String stringToDecompress){
+        // If string is empty returning empty string.
+        int stringLength = stringToDecompress.length();
+        if (stringLength == 0) {
+            return "";
+        }
+
+        String tempChar = "";
+        int tempCount = 0;
+        String result = "";
+        for (int i = 0; i < stringLength - 1; i+=2) {
+            tempChar = String.valueOf(stringToDecompress.charAt(i));
+            tempCount = Integer.parseInt(String.valueOf(stringToDecompress.charAt(i+1)));
+            result = result.concat(tempChar.repeat(tempCount));
+        }
+
+        return result;
+    }
+
 }
